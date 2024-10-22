c2414f326e5ca3a36bf20dd858d247ccbbfba1c0
gothmate <gothmate@gmail.com>
2024-10-22 11:06:25 -0300

    fixes

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header>
             <h1>Bem vindo ao jogo da Forca!</h1>
             <h3>Seu objetivo é acertar o nome do filme abaixo.</h3>
-            <p>Você tem um total de 6 chances</p>
+            <p>Você tem um total de 5 chances</p>
         </header>
     )
 }
